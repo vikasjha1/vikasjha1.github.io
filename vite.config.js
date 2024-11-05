d1254a5499b4bc2e3889dacf1e986deabf2a163d
vikasjha1 <111581325+vikasjha1@users.noreply.github.com>
2024-11-05 11:59:58 +1030

    Set base in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/vikasjha1.github.io/', // Set this to your GitHub Pages repo name
+});
