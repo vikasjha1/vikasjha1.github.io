2c7cf610155410bfd4ab8dbc0417ae2a40b3ba4a
vikasjha1 <111581325+vikasjha1@users.noreply.github.com>
2024-11-05 13:13:24 +1030

    updated base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vikasjha1.github.io/', // Set this to your GitHub Pages repo name
+  base: '/', // Set this to your GitHub Pages repo name
 });
